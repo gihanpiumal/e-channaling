17c0f32c47710f3bf66d32647a42c21acfab45e4
Your Namegihan79097 <gihanpiumal12345@gmail.com>
2022-07-12 22:41:09 +0530

    make the modals for homepage

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
 
-import { Card, Col, Row, Button, Space, Avatar } from "antd";
-import { PlayCircleOutlined, UserOutlined } from "@ant-design/icons";
+import {
+  Col,
+  Row,
+  Button,
+  Space,
+  Avatar,
+  Modal,
+  DatePicker,
+  Select,
+  Input,
+  Table,
+} from "antd";
+import {
+  PlayCircleOutlined,
+  UserOutlined,
+  EyeOutlined,
+} from "@ant-design/icons";
 
 import "./HomePage.css";
 
@@ -11,13 +26,267 @@ import headerPcrImage from "../../images/pcr.png";
 import headerDoctorImage from "../../images/doctor.png";
 import headerMedicineImage from "../../images/medicine.png";
 
+const { Option } = Select;
+const { Search } = Input;
+
 export class HomePage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isPcrModalVisible: false,
+      isFindDoctorsModalVisible: false,
+      isFindMedicineModalVisible: false,
+      isChannelSpecializeModalVisible: false,
+    };
+  }
   aaa = () => {
     console.log(">>>>>>>>");
   };
+
+  ////////////////////////////////// modal controls //////////////////////////////
+
+  showPcrModal = () => {
+    this.setState({ isPcrModalVisible: true });
+  };
+  showFindDoctorModal = () => {
+    this.setState({ isFindDoctorsModalVisible: true });
+  };
+  showFindMedicineModal = () => {
+    this.setState({ isFindMedicineModalVisible: true });
+  };
+  showChannelSpecializeModal = () => {
+    this.setState({ isChannelSpecializeModalVisible: true });
+  };
+
+  handleCancel = () => {
+    this.setState({ isPcrModalVisible: false });
+    this.setState({ isFindDoctorsModalVisible: false });
+    this.setState({ isFindMedicineModalVisible: false });
+    this.setState({ isChannelSpecializeModalVisible: false });
+  };
+
+  ////////////////////////////////// modal controls //////////////////////////////
+
+  showActions = (record) => {
+    return (
+      <EyeOutlined
+        className="action-icons"
+        // onClick={() => this.showEditModal(record)}
+      />
+    );
+  };
+
+  /////////////////////// tempory data  //////////////////////////
+  columns = [
+    {
+      title: "Specialization",
+      dataIndex: "specialization",
+      key: "specialization",
+    },
+    {
+      title: "Doctor Name",
+      dataIndex: "doctor_name",
+      key: "doctor_name",
+    },
+    {
+      title: "Action",
+      key: "action",
+      width: "10%",
+      render: (_, record) => this.showActions(record),
+    },
+  ];
+
+  data = [
+    {
+      key: "1",
+      specialization: "Physiologist",
+      doctor_name: "John Brown",
+      tags: ["nice", "developer"],
+    },
+  ];
+
+  /////////////////////// tempory data  //////////////////////////
+
   render() {
+    const {
+      isPcrModalVisible,
+      isFindDoctorsModalVisible,
+      isFindMedicineModalVisible,
+      isChannelSpecializeModalVisible,
+    } = this.state;
     return (
       <div className="homepage-wrapper">
+        {/* Modal for PCR apoiment */}
+        <Modal
+          title="PCR Apoiment"
+          visible={isPcrModalVisible}
+          footer={null}
+          onCancel={this.handleCancel}
+        >
+          {/* <Space direction="vertical"> */}
+          <Row>
+            <Col span={10}>
+              <p>Date :</p>
+            </Col>
+            <Col span={14}>
+              <DatePicker />
+            </Col>
+          </Row>
+          <Row>
+            <Col span={10}>
+              <p>Available Times :</p>
+            </Col>
+            <Col span={14}>
+              <Select
+                defaultValue="lucy"
+                style={{ width: 120 }}
+                // onChange={handleChange}
+              >
+                <Option value="jack">Jack</Option>
+                <Option value="lucy">Lucy</Option>
+              </Select>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={10}>
+              <p>Price :</p>
+            </Col>
+            <Col span={14}>
+              <h3>Rs. 6500.00</h3>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={10}>
+              <p>Your Token is :</p>
+            </Col>
+            <Col span={14}>
+              <h3>12</h3>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={10}>
+              <Button>Cancel</Button>
+            </Col>
+            <Col span={14}>
+              <Button>Save and Genarate PDF</Button>
+            </Col>
+          </Row>
+        </Modal>
+        {/* Modal for PCR apoiment */}
+
+        {/* Modal for find doctor apoiment */}
+
+        <Modal
+          title="Find Doctor"
+          visible={isFindDoctorsModalVisible}
+          footer={null}
+          onCancel={this.handleCancel}
+        >
+          <Row>
+            <Col span={10}>
+              <p>Search</p>
+            </Col>
+            <Col span={14}>
+              <Search placeholder="search by name" style={{ width: 200 }} />
+            </Col>
+          </Row>
+          <Row>
+            <Col span={10}>
+              <p>Filter by specialization :</p>
+            </Col>
+            <Col span={14}>
+              <Select
+                defaultValue=""
+                style={{ width: 120 }}
+                // onChange={handleChange}
+              >
+                <Option value="jack">Jack</Option>
+                <Option value="lucy">Lucy</Option>
+              </Select>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={12}>
+              <Button>Filter</Button>
+            </Col>
+            <Col span={12}>
+              <Button>Clear Filter</Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={24}>
+              <Table columns={this.columns} dataSource={this.data} />
+            </Col>
+          </Row>
+        </Modal>
+
+        {/* Modal for find doctor apoiment */}
+
+        {/* Modal for find medicine */}
+
+        <Modal
+          title="Find Medicine"
+          visible={isFindMedicineModalVisible}
+          footer={null}
+          onCancel={this.handleCancel}
+        >
+          <Row>
+            <Col span={10}>
+              <p>Search</p>
+            </Col>
+            <Col span={14}>
+              <Search placeholder="search by name" style={{ width: 200 }} />
+            </Col>
+          </Row>
+          <Row>
+            <Col span={12}>
+              <Button>Filter</Button>
+            </Col>
+            <Col span={12}>
+              <Button>Clear Filter</Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={24}>
+              <Table columns={this.columns} dataSource={this.data} />
+            </Col>
+          </Row>
+        </Modal>
+        {/* Modal for find medicine */}
+
+        {/* Modal for Channnel Specialize */}
+
+        <Modal
+          title="Channel Specialize"
+          visible={isChannelSpecializeModalVisible}
+          footer={null}
+          onCancel={this.handleCancel}
+        >
+          <Row>
+            <h3>Physician</h3>
+          </Row>
+          <Row>
+            <h1>IMAGE</h1>
+          </Row>
+          <Row>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quasi
+              dolore obcaecati placeat beatae, laborum eos molestias quidem
+              similique voluptate veritatis atque consequatur maiores corporis?
+              Temporibus earum voluptas dignissimos dolorum.
+            </p>
+          </Row>
+          <Row>
+            <h2>Available Doctors</h2>
+          </Row>
+          <Row>
+            <Col span={24}>
+              <Table columns={this.columns} dataSource={this.data} />
+            </Col>
+          </Row>
+        </Modal>
+        {/* Modal for Channnel Specialize */}
         <div className="homepage-header">
           <Row>
             <Col span={12}>
@@ -91,7 +360,7 @@ export class HomePage extends Component {
           <div className="homepage-middle-qucikaccess-cards">
             <Row span={24}>
               <Row span={8}>
-                <div className="pcr-card" onClick={this.aaa}>
+                <div className="pcr-card" onClick={this.showPcrModal}>
                   <div
                     className="pcr-card-img"
                     style={{ backgroundImage: `url(${headerPcrImage})` }}
@@ -101,7 +370,10 @@ export class HomePage extends Component {
                 </div>
               </Row>
               <Row span={8}>
-                <div className="find-doctor-card" onClick={this.aaa}>
+                <div
+                  className="find-doctor-card"
+                  onClick={this.showFindDoctorModal}
+                >
                   <div
                     className="find-doctor-card-img"
                     style={{ backgroundImage: `url(${headerDoctorImage})` }}
@@ -111,7 +383,10 @@ export class HomePage extends Component {
                 </div>
               </Row>
               <Row span={8}>
-                <div className="find-medicine-card" onClick={this.aaa}>
+                <div
+                  className="find-medicine-card"
+                  onClick={this.showFindMedicineModal}
+                >
                   <div
                     className="find-medicine-card-img"
                     style={{ backgroundImage: `url(${headerMedicineImage})` }}
@@ -136,7 +411,10 @@ export class HomePage extends Component {
 
           <Row span={24}>
             <Row span={8}>
-              <div className="top-specialist-card-1">
+              <div
+                className="top-specialist-card-1"
+                onClick={this.showChannelSpecializeModal}
+              >
                 <div className="top-specialist-card-avatar">
                   <Avatar
                     size={64}
@@ -150,7 +428,10 @@ export class HomePage extends Component {
               </div>
             </Row>
             <Row span={8}>
-              <div className="top-specialist-card-2">
+              <div
+                className="top-specialist-card-2"
+                onClick={this.showChannelSpecializeModal}
+              >
                 <div className="top-specialist-card-avatar">
                   <Avatar
                     size={64}
@@ -164,7 +445,10 @@ export class HomePage extends Component {
               </div>
             </Row>
             <Row span={8}>
-              <div className="top-specialist-card-3">
+              <div
+                className="top-specialist-card-3"
+                onClick={this.showChannelSpecializeModal}
+              >
                 <div className="top-specialist-card-avatar">
                   <Avatar
                     size={64}
